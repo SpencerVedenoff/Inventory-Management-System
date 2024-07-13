5c04c85d12fec88dcc4251740d1e35a5d78aacb3
SpencerVedenoff <115957432+SpencerVedenoff@users.noreply.github.com>
2024-07-13 12:05:45 -0500

    Add files via upload

---
+++ C968/C968/C968/Validator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C968
+{
+    abstract class Validator
+    {
+        protected string ErrorMessage { get; set; }
+
+        public Validator()
+        {
+            ErrorMessage = "";
+        }
+
+        public static bool IsInt(string s)
+        {
+            return int.TryParse(s, out int i);
+        }
+        public static bool IsDecimal(string s)
+        {
+            return decimal.TryParse(s, out decimal i);
+        }
+        public void Show()
+        {
+            MessageBox.Show(ErrorMessage, "Error");
+        }
+    }
+
+}
