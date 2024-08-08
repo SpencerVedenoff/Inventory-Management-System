eae63b576213ed623acff87b52215eca79120e61
SpencerVedenoff <115957432+SpencerVedenoff@users.noreply.github.com>
2024-08-07 18:20:49 -0700

    Add files via upload

---
+++ C968-main - Copy/C968/C968/C968/Program.cs
@@ -0,0 +1,12 @@
+namespace C968
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new MainForm());
+        }
+    }
+}
